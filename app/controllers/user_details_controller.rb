6e2d1e31460230fcfa86a19b91ee6aebf07bbd3d
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-29 15:27:07 +0900

    詳細情報登録画面の作成

---
+++ app/controllers/user_details_controller.rb
@@ -0,0 +1,19 @@
+class UserDetailsController < ApplicationController
+  def new
+    @user_detail = UserDetail.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def set_user_detail
+    
+  end
+end
