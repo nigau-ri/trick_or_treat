9455a4e0e3968c07bc6301f206ee8168bc0e88bd
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-29 17:13:17 +0900

    ユーザー詳細情報の登録機能の実装

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -4,16 +4,29 @@ class UserDetailsController < ApplicationController
   end
 
   def create
+    @user_detail = UserDetail.new(user_detail_params)
+    if @user_detail.save
+      
+    else
+      render :new
+    end
   end
 
   def edit
+    @user_detail = current_user.user_detail
   end
 
   def update
+    if current_user.user_detail.update(user_detail_params)
+      
+    else
+      @user_detail = current_user.user_detail
+      render :edit
+    end
   end
 
   private
-  def set_user_detail
-    
+  def user_detail_params
+    params.require(:user_detail).permit(:age, :gender_id, :prefecture_id, :city, :hobby).merge(user_id: current_user.id)
   end
 end
