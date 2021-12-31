5e8d609ee245424723b5a1c567d87e21b2df54dd
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-31 12:28:59 +0900

    タグ作成機能の実装

--- app/controllers/following_tags_controller.rb
+++ app/controllers/following_tags_controller.rb
@@ -1,2 +1,17 @@
 class FollowingTagsController < ApplicationController
+  def create
+    @following_tag = FollowingTag.new(tag_params)
+    if @following_tag.save
+      redirect_to following_path(params[:following_id])
+    else
+      @following = Following.find(params[:following_id])
+      @following_user = User.find(@following.following_id)
+      render 'followings/show'
+    end
+  end
+
+  private
+  def tag_params
+    params.require(:following_tag).permit(:name)
+  end
 end
