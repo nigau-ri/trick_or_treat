f2047821b9f5b593cf41126bd75cd26c6639f9d9
nigau-ri <akihito.yoshizawa@gmail.com>
2022-01-01 14:11:31 +0900

    マイページへのリンクを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'home/show', to: 'home#show'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update] do
     resources :user_details, only: [:new, :create, :edit, :update]
