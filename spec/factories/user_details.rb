ddc2f3fce83ee05a75c6d2856ec349a5fb320711
nigau-ri <akihito.yoshizawa@gmail.com>
2022-01-14 14:06:21 +0900

    ユーザー詳細情報新規登録のテストコードの作成

--- spec/factories/user_details.rb
+++ spec/factories/user_details.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user_detail do
-    
+    age           {Faker::Number.between(from: 20, to: 100)}
+    gender_id     {Faker::Number.between(from: 1, to: 2)}
+    prefecture_id {Faker::Number.between(from: 1, to: 47)}
+    city          {Gimei.unique.city.kanji}
+    hobby         {Faker::Hobby.activity}
+    association :user
   end
 end
--- spec/models/user_detail_spec.rb
+++ spec/models/user_detail_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe UserDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user_detail = FactoryBot.build(:user_detail)
+  end
+
+  describe 'ユーザー詳細情報新規登録' do
+    context '新規登録できるとき' do
+      it '年齢と性別と都道府県と市町村と趣味が存在すれば登録できる' do
+        expect(@user_detail).to be_valid
+      end
+      it '年齢は空でも登録できる' do
+        @user_detail.age = ""
+        expect(@user_detail).to be_valid
+      end
+      it '都道府県は空でも登録できる' do
+        @user_detail.prefecture_id = ""
+        expect(@user_detail).to be_valid
+      end
+      it '市町村は空でも登録できる' do
+        @user_detail.city = ""
+        expect(@user_detail).to be_valid
+      end
+      it '趣味は空でも登録できる' do
+        @user_detail.hobby = ""
+        expect(@user_detail).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it '性別が空では登録できない' do
+        @user_detail.gender_id = ""
+        @user_detail.valid?
+        expect(@user_detail.errors.full_messages).to include("性別を入力してください")
+      end
+      it 'userが空では登録できない' do
+        @user_detail.user = nil
+        @user_detail.valid?
+        expect(@user_detail.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
 end
