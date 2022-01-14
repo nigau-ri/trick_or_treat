069bb17f6d4115236f3793e56f9e697b9030e198
nigau-ri <akihito.yoshizawa@gmail.com>
2022-01-14 17:32:17 +0900

    イベント検索フォームの修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if params[:q]&.dig(:room_detail_place)
+      squished_keywords = params[:q][:room_detail_place].squish
+      params[:q][:room_detail_place_cont_any] = squished_keywords.split(" ")
+    end
     @q = Room.where(matched: 'yet').ransack(params[:q])
     @rooms = @q.result
   end
